78e0a7df3446ad27061f8a586203ccdbe8dc7a03
Hans362 <i@hans362.cn>
2021-03-14 14:27:15 +0800

    Personal homepage beta v0.0.5

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
     <title>Hans362</title>
     <meta http-equiv="x-dns-prefetch-control " content="on">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
-    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1"/>
+    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1" />
     <meta name="description" content="Hans362 's Home | Personal HomePage | Hans362 的个人主页">
     <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/favicon-32x32.png">
@@ -23,87 +23,93 @@
 
 <body style="background-image: url('https://cdn.jsdelivr.net/gh/Dreamer-Paul/Sweet@master/static/background-3.jpg')">
 
-<div id="loader">
-    <figure>
-        <img src="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/2021/03/13/1368931.jpg"/>
-    </figure>
-</div>
-
-<main>
-    <div class="content">
-        <figure class="me">
-            <img class="avatar" src="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/2021/03/13/1368931.jpg"/>
-            <h2 class="name">Hans362</h2>
+    <div id="loader">
+        <figure>
+            <img src="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/2021/03/13/1368931.jpg" />
         </figure>
-        <section id="main" class="active">
-            <a href="https://blog.hans362.cn"><i class="fa fa-wordpress"></i><span class="title">博客</span></a>
-            <a href="https://twitter.com/Milk_362" target="_blank"><i class="fa fa-twitter"></i><span class="title">Twitter</span></a>
-            <a href="https://t.me/hans362" target="_blank"><i class="fa fa-telegram"></i><span class="title">Telegram</span></a>
-            <a href="mailto:i@hans362.cn" target="_blank"><i class="fa fa-envelope"></i><span class="title">邮箱</span></a>
-            <a href="https://github.com/hans362" target="_blank"><i class="fa fa-github"></i><span class="title">GitHub</span></a>
-            <a href="https://space.bilibili.com/66745436" target="_blank"><i class="fa fa-flag"></i><span class="title">哔哩哔哩</span></a>
-        </section>
-        <section id="articles">
-<?php
-//error_reporting(0);
+    </div>
 
-// 设置你的博客 RSS 地址（WordPress、Typecho 均可）
-define("BLOG_URL", "https://blog.hans362.cn/atom.xml");
-
-$file = simplexml_load_file(BLOG_URL) -> channel -> item;
-
-if(isset($file)){
-    for($i = 0; $i < 6; $i++){
-        $timestamp = strtotime($file[$i] -> pubDate);
-        $timestamp = date("y-m-d", $timestamp);
-
-        if($file[$i]){
-            echo '<a href="' . $file[$i] -> link . '" target="_blank">' .$file[$i] -> title . '<span class="meta">' . $timestamp . "</span></a>";
-            echo "\n";
-        }
-        else{
-            break;
-        }
-    }
-}
-else{
-    echo "<a>博客连接失败<span class='meta'>请检查</span></a>";
-}
-
-?>
-        </section>
-        <section id="products">
-            <a class="item" href="" target="_blank">Example<span class="meta">Example</span></a>
-        </section>
-        <section id="about">
-            <p>待填坑<a href="" target="_blank">详细阅读</a></p>
-        </section>
-        <div class="actions">
-            <div class="item home active">
-                <i class="fa fa-star"></i>
-                <span class="title">首页</span>
-            </div>
-            <div class="item article">
-                <i class="fa fa-book"></i>
-                <span class="title">文章</span>
-            </div>
-            <div class="item product">
-                <i class="fa fa-trophy"></i>
-                <span class="title">作品</span>
-            </div>
-            <div class="item about">
-                <i class="fa fa-graduation-cap"></i>
-                <span class="title">关于</span>
+    <main>
+        <div class="content">
+            <figure class="me">
+                <img class="avatar" src="https://cdn.jsdelivr.net/gh/hans362/image-hosting@master/2021/03/13/1368931.jpg" />
+                <h2 class="name">Hans362</h2>
+            </figure>
+            <section id="main" class="active">
+                <a href="https://blog.hans362.cn"><i class="fa fa-wordpress"></i><span class="title">博客</span></a>
+                <a href="https://lab.hans362.cn" target="_blank"><i class="fa fa-flask"></i><span class="title">实验室</span></a>
+                <a href="https://status.hans362.cn" target="_blank"><i class="fa fa-check-circle"></i><span class="title">服务状态</span></a>
+                <a href="https://t.me/hans362" target="_blank"><i class="fa fa-telegram"></i><span class="title">Telegram</span></a>
+                <a href="https://t.me/Hans362sTeleblog" target="_blank"><i class="fa fa-paper-plane"></i><span class="title">Teleblog</span></a>
+                <a href="https://twitter.com/Milk_362" target="_blank"><i class="fa fa-twitter"></i><span class="title">Twitter</span></a>
+                <a href="https://github.com/hans362" target="_blank"><i class="fa fa-github"></i><span class="title">GitHub</span></a>
+                <a href="https://space.bilibili.com/66745436" target="_blank"><i class="fa fa-flag"></i><span class="title">哔哩哔哩</span></a>
+                <a href="mailto:i@hans362.cn" target="_blank"><i class="fa fa-envelope"></i><span class="title">邮箱</span></a>
+            </section>
+            <section id="articles">
+                <?php
+                error_reporting(0);
+                // 设置你的博客 RSS 地址（Hexo Only）
+                define("BLOG_URL", "https://blog.hans362.cn/atom.xml");
+                $file = simplexml_load_file(BLOG_URL);
+                if (isset($file)) {
+                    for ($i = 0; $i < 8; $i++) {
+                        $timestamp = strtotime($file->children()->entry[$i]->published);
+                        $timestamp = date("y-m-d", $timestamp);
+                        if ($file[$i]) {
+                            echo '<a href="' . $file->children()->entry[$i]->children()->link->attributes()['href'] . '" target="_blank">' . $file->children()->entry[$i]->children()->title . '<span class="meta">' . $timestamp . "</span></a>";
+                            echo "\n";
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    echo "<a>博客连接失败<span class='meta'>请检查</span></a>";
+                }
+                ?>
+            </section>
+            <section id="products">
+                <?php
+                $json = file_get_contents("https://gh-pinned-repos.now.sh/?username=hans362");
+                $repos = json_decode($json);
+                for ($i = 0; $i < 6; $i++) {
+                    if ($repos[$i]->owner != "hans362") {
+                        echo '<a class="item" href="https://github.com/' . $repos[$i]->owner . '/' . $repos[$i]->repo . '" target="_blank">' . $repos[$i]->owner . '/' . $repos[$i]->repo . '<span class="meta">' . $repos[$i]->language . '</span></a>';
+                    } else {
+                        echo '<a class="item" href="https://github.com/hans362/' . $repos[$i]->repo . '" target="_blank">' . $repos[$i]->repo . '<span class="meta">' . $repos[$i]->language . '</span></a>';
+                    }
+                }
+                ?>
+            </section>
+            <section id="about">
+                <p>待填坑<a href="" target="_blank">详细阅读</a></p>
+            </section>
+            <div class="actions">
+                <div class="item home active">
+                    <i class="fa fa-star"></i>
+                    <span class="title">首页</span>
+                </div>
+                <div class="item article">
+                    <i class="fa fa-book"></i>
+                    <span class="title">文章</span>
+                </div>
+                <div class="item product">
+                    <i class="fa fa-trophy"></i>
+                    <span class="title">作品</span>
+                </div>
+                <div class="item about">
+                    <i class="fa fa-graduation-cap"></i>
+                    <span class="title">关于</span>
+                </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
-<footer>
-    <p>© 2017-2021 By <a href="https://hans362.cn">Hans362</a> & Based on <a href="https://paugram.com">Dreamer-Paul</a>'s design.</p>
-</footer>
+    <footer>
+        <p>© 2017-2021 By <a href="https://hans362.cn">Hans362</a> & Based on <a href="https://paugram.com">Dreamer-Paul</a>'s design.</p>
+    </footer>
 
-<script src="https://cdn.jsdelivr.net/gh/Dreamer-Paul/Sweet@master/static/paul.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/Dreamer-Paul/Sweet@master/static/paul.js"></script>
 
 </body>
 
